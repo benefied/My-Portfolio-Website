5e78f027dd3b359cb06620ab13b72f2977867206
unknown <ojekudobenedict1@gmail.com>
2023-08-03 21:20:48 -1100

    fixed the collapsible nav and set the loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Heading from './components/Heading'
 import Portfolio from './components/Portfolio'
 import Skills from './components/Skills'
+import SyncLoader from "react-spinners/SyncLoader";
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(()=>{
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 2000);
+  }, [])
   return (
     <>
+      {
+        isLoading ?
+        <div className="appLogo__Area">
+        <div className='appLogo__logo'>
+        <SyncLoader
+        color={"rgba(21, 146, 90, 1)"}
+        loading={isLoading}
+        size={50}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+        </div>
+      </div>
+        :
+      <>
       <Navbar/>
       <Heading/>
       <Portfolio/>
       <Skills/>
+      </>
+      }
     </>
   )
 }
--- src/components/CollapseNav.jsx
+++ src/components/CollapseNav.jsx
@@ -5,7 +5,7 @@ const CollapseNav = () => {
   return (
     <div className='bg-white'>
         <div className='pt-[4rem] px-3'>
-            <h1 className='text-center text-green-600'>Easy Slide</h1>
+            <h1 className='text-center text-green-600'>Navigator</h1>
             <ul className=''>
                 <li className='font-bold text-gray-600 text-xl border-b-4'><button className='cursor-pointer'>About</button></li>
                 <li className='font-bold text-gray-600 text-xl border-b-4'><button className='cursor-pointer'>Portfolio</button></li>
